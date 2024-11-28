e9b3ad5a0082fd879120dbd67b5716b7f5f26c4b
Rubio <rubiodainton@gmail.com>
2024-11-28 12:30:25 -0300

    Metodos getById, getByTitulo, post, put, delete

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,30 +1,76 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagens;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
-@RestController // Deﬁne que a Classe é do tipo RestController que receberá requisições composta por :URL,Verbo Método HTTP, Body - corpo com dados ou não
-@RequestMapping("/postagens")// Essa anotação indica que as requisições HTTP direcionadas para /postagens serão tratadas pelo método ou classe que ela anota.
-@CrossOrigin(origins="*",allowedHeaders="*")
+import jakarta.validation.Valid;
+
+@RestController // Deﬁne que a Classe é do tipo RestController que receberá requisições composta
+				// por :URL,Verbo Método HTTP, Body - corpo com dados ou não
+@RequestMapping("/postagens") // Essa anotação indica que as requisições HTTP direcionadas para /postagens
+								// serão tratadas pelo método ou classe que ela anota.
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
-	
-	@Autowired
+
+	@Autowired // injeção de dependecnia
 	private PostagemRepository postagemRepository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Postagens>> getAll(){
+	public ResponseEntity<List<Postagens>> getAll() {
 		return ResponseEntity.ok(postagemRepository.findAll());
-		
+
 	}
-	
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagens> getById(@PathVariable Long id) {
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	@GetMapping("titulo/{titulo}")
+	public ResponseEntity<List<Postagens>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+
+	@PostMapping
+	public ResponseEntity<Postagens> post(@Valid @RequestBody Postagens postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+
+	@PutMapping
+	public ResponseEntity<Postagens> put(@Valid @RequestBody Postagens postagem) {
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagens> postagem = postagemRepository.findById(id);
+
+		if (postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND); // erro nada encontrado
+		postagemRepository.deleteById(id);
+	}
+
 }
